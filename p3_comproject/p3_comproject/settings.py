a1bf911cc5c31b1250122bd7012a86045c728062
Gaurav Bankar <140523859+grvbnkr@users.noreply.github.com>
2026-01-28 10:21:32 +0530

    Update settings.py

--- p3_comproject/p3_comproject/settings.py
+++ p3_comproject/p3_comproject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,4 +122,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
